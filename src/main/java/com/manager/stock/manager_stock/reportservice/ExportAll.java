52e4df857ee7144cac9d3abeaf9375dba4319e74
Tronghuong3110 <tronghuong2002@gmail.com>
2026-01-05 00:33:57 +0700

    fix error

--- src/main/java/com/manager/stock/manager_stock/reportservice/ExportAll.java
+++ src/main/java/com/manager/stock/manager_stock/reportservice/ExportAll.java
@@ -22,10 +22,6 @@ public class ExportAll {
     Sheet sheet = workbook.createSheet("Export");
     Map<String, Integer> receiptPosMap = new HashMap<>();
 
-    public ExportAll(int selectedYear) {
-
-    }
-
     public void exportTotal(String pathFile) {
         createTitleRow();
         fillData();
